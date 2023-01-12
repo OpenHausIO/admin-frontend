eb887a260aeac893555b09a37d45aa09eb992a8d
Marc Stirner <marc.stirner@it2media.de>
2023-01-12 16:18:30 +0100

    stuff

--- src/views/admin.store.vue
+++ src/views/admin.store.vue
@@ -11,84 +11,93 @@ import ActionsButtons from "@/components/ActionsButtons.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  data() {
-    return {
-      editItem: null,
-      tabItems: [
-        {
-          name: "Overview",
-          id: "overview",
-        },
-        {
-          name: "Add",
-          id: "add",
-        },
-      ],
-    };
-  },
-  computed: {
-    config() {
-      return store.state.store;
+    data() {
+        return {
+            editItem: null,
+            tabItems: [
+                {
+                    name: "Overview",
+                    id: "overview",
+                },
+                {
+                    name: "Add",
+                    id: "add",
+                },
+            ],
+        };
     },
-  },
-  methods: {
-    handleEdit(item) {
-      if (this.editItem === item._id) {
-        this.editItem = null;
-      } else {
-        this.editItem = item._id;
-      }
+    computed: {
+        config() {
+            return store.state.store;
+        },
     },
-    handleEdit() {},
-    handleInfo() {},
-    handleRemove() {},
-  },
+    methods: {
+        handleEdit(item) {
+            if (this.editItem === item._id) {
+                this.editItem = null;
+            } else {
+                this.editItem = item._id;
+            }
+        },
+        handleInfo() { },
+        handleRemove() { },
+    },
 });
 </script>
 
 
 <template>
-  <div>
-    <Tabs v-bind:items="tabItems">
-      <template v-slot:overview>
-        <table class="table text-white">
-          <thead>
-            <tr>
-              <th scope="col">Config</th>
-              <th scope="col">Namespace (Item)</th>
-              <th scope="col" style="width: 10px">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-bind:key="item._id" v-for="item in config">
-              <td>
-                <!--{{ item }}-->
+    <div>
+        <Tabs v-bind:items="tabItems">
+            <template v-slot:overview>
+                <table class="table text-white">
+                    <thead>
+                        <tr>
+                            <th scope="col">Config</th>
+                            <th scope="col">Namespace (Item)</th>
+                            <th scope="col" style="width: 10px">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-bind:key="item._id" v-for="item in config">
+                            <td>
 
-                <ul class="ps-3">
-                  <li v-bind:key="index" v-for="(config, index) in item.config">
-                    {{ config.key }} = {{ config.value }}
-                  </li>
-                </ul>
-              </td>
-              <td>{{ item.namespace }} ({{ item.item }})</td>
-              <td>
-                <ActionsButtons
-                  :showEdit="true"
-                  :showInfo="true"
-                  :showRemove="true"
-                  :item="item"
-                  @handleEdit="handleEdit"
-                  @handleInfo="handleInfo"
-                  @handleRemove="handleRemove"
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </template>
-      <template v-slot:add> Hello from apsdflkasjfdlasdf </template>
-    </Tabs>
+                                <table class="table text-white">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Key</th>
+                                            <th scope="col">Value</th>
+                                            <th scope="col">Description</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-bind:key="index" v-for="(config, index) in item.config">
+                                            <td>
+                                                {{ config.key }} =
+                                            </td>
+                                            <td>
+                                                {{ config.value }}
+                                            </td>
+                                            <td>
+                                                {{ config.description }}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
 
-    <!--store: {{ store }}-->
-  </div>
+                            </td>
+                            <td>{{ item.namespace }} ({{ item.item }})</td>
+                            <td>
+                                <ActionsButtons :showEdit="true" :showInfo="true" :showRemove="true" :item="item"
+                                    @handleEdit="handleEdit" @handleInfo="handleInfo" @handleRemove="handleRemove" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </template>
+            <template v-slot:add> Hello from apsdflkasjfdlasdf </template>
+        </Tabs>
+
+        <!--store: {{ store }}-->
+    </div>
 </template>

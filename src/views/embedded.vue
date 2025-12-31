62895553ed92d238294322b81211d515270a187a
Marc Stirner <marc.stirner@hotmail.de>
2025-12-31 14:13:24 +0100

    added

---
+++ src/views/embedded.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    src: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="conatiner">
+        <div class="row">
+            <div class="col-12 p-0">
+                <Iframe class="vh-100 w-100" :src="src" />
+            </div>
+        </div>
+    </div>
+</template>

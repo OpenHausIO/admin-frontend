7e120c14b1c1b1dfddcd29019e361bebd04eaf65
Marc Stirner <marc.stirner@hotmail.de>
2026-01-28 18:30:17 +0100

    set host header & handle /about.json request

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from "url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
+import { config } from "dotenv";
+config();
+
 const {
     BACKEND_HOST,
     BACKEND_PORT,
@@ -14,12 +17,24 @@ const {
 }, process.env);
 
 
+console.log("URL", `${BACKEND_PROTOCOL}://${BACKEND_HOST}:${BACKEND_PORT}`);
+
+const aboutJson = () => ({
+    configureServer(server) {
+        server.middlewares.use("/admin/about.json", (req, res, next) => {
+            const { version } = require("./package.json");
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify({ version }));
+        });
+    },
+});
+
 const redirect = () => ({
     configureServer(server) {
         server.middlewares.use("/user", (req, res, next) => {
 
             res.writeHead(302, {
-                Location: 'http://localhost:3000/user/'
+                Location: "http://localhost:3001/user/"
             });
 
             res.end();
@@ -34,6 +49,7 @@ export default defineConfig({
     base: "/admin/",
     plugins: [
         vue(),
+        aboutJson(),
         redirect()
     ],
     resolve: {
@@ -42,15 +58,27 @@ export default defineConfig({
         }
     },
     server: {
-        port: 3001,
+        port: 3002,
         host: "0.0.0.0",
         proxy: {
             "/api": {
                 target: `${BACKEND_PROTOCOL}://${BACKEND_HOST}:${BACKEND_PORT}`,
-                ws: true
+                ws: true,
+                xfwd: true,
+                configure: (proxy) => {
+                    proxy.on("proxyReq", (proxyReq) => {
+                        proxyReq.setHeader("Host", `${BACKEND_HOST}:${BACKEND_PORT}`);
+                    });
+                }
             },
             "/auth": {
-                target: `${BACKEND_PROTOCOL}://${BACKEND_HOST}:${BACKEND_PORT}`
+                target: `${BACKEND_PROTOCOL}://${BACKEND_HOST}:${BACKEND_PORT}`,
+                xfwd: true,
+                configure: (proxy) => {
+                    proxy.on("proxyReq", (proxyReq) => {
+                        proxyReq.setHeader("Host", `${BACKEND_HOST}:${BACKEND_PORT}`);
+                    });
+                }
             }
         }
     },
